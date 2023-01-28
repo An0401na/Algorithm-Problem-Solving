35ad9337612776ab6a8ec4c383a58d41d85333ec
An Na <99172832+An0401na@users.noreply.github.com>
2023-01-29 06:17:29 +0900

    [Bronze II] Title: 진수 변환기, Time: 212 ms, Memory: 17788 KB -BaekjoonHub

--- 백준/Bronze/14915. 진수 변환기/진수 변환기.java
+++ 백준/Bronze/14915. 진수 변환기/진수 변환기.java
@@ -11,6 +11,13 @@ public class Main {
 		String [] abc = {"A","B","C","D","E","F"};
 		
 		int i =0; //지수
+		
+		//10진수 m이 0이라면 n진수로 변환시 모두 0
+		if(m==0) {
+			result="0";
+		}
+		
+		//10진수 m을 n진수로 표현하기 위해서 몇자리수까지 필요한지 체크
 		while (true) {
 			if((int)(m/Math.pow(n, i))>0) {
 				i++;
@@ -18,13 +25,12 @@ public class Main {
 				break;
 			}
 		}
-		if(m==0) {
-			result="0";
-		}
+		
+		
 		for (int j = i-1; j >=0; j--) {
 			if(m>=Math.pow(n,j)) {
-				if((int)(m/Math.pow(n, j))>9) {
-					result = result+abc[(int)(m/Math.pow(n, j))%10];
+				if((int)(m/Math.pow(n, j))>9) { //n이 10이상일때부터 나누는 몫이 10이상이 나올수 있다.
+					result = result+abc[(int)(m/Math.pow(n, j))%10]; //알파벳으로 표현해야하는경우
 					m=(int) (m%Math.pow(n, j));
 				}else {
 					result = result+Integer.toString((int) (m/Math.pow(n, j)));
