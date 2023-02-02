7d5303571170ebe834d857d29794ef0c772be94e
An Na <99172832+An0401na@users.noreply.github.com>
2023-02-02 23:54:04 +0900

    Update N과 M （5）.java
    주석 추가

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -34,11 +34,20 @@ public class Main {
 		}
 		
 		for (int i = 0; i < N; i++) {
+			// 이전에 뽑지 않았던 수라면 
 			if(!visited[i]) {
+				//뽑았다고 표시
 				visited[i]=true;
+				//뽑은 수 추가
 				result.add(num[i]);
 				dfs(depth+1, i);
+				// ex ) 현시점 result 1 2 3 4 - - 
 				result.remove(result.size()-1);
+				// ex ) 현시점 result 1 2 3 - - -
+				// 1 2 3 4 일때 나머지 두자릴 다 탐색하고 네번째 자리를 4 다음큰 숫자로 변경하기위해서
+				//result에 가장 마지막에 들어있는 수를 제거한고
+				
+				//네번째 자리가 4 다음 큰수가 들어왔을때 탐색했던 숫자들은 방문하지 않아야 하므로 다시 false로 변경
 				visited [i]= false;
 			}
 		}
