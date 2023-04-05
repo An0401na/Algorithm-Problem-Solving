92afe3bda7ec9d2d567b87755a8e02f430667403
An Na <99172832+An0401na@users.noreply.github.com>
2023-04-05 10:56:28 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 3,268 ms, Memory: 110,496 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -10,6 +10,7 @@ public class Solution {
 	static int count;
 	static int A[];
 	static int B[];
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		T = Integer.parseInt(br.readLine());
@@ -18,11 +19,11 @@ public class Solution {
 			D = Integer.parseInt(st.nextToken());
 			W = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
-			
+
 			count = Integer.MAX_VALUE;
-			map = new int [D][W];
+			map = new int[D][W];
 			A = new int[W];
-			B = new int [W];
+			B = new int[W];
 			for (int i = 0; i < D; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < W; j++) {
@@ -32,103 +33,68 @@ public class Solution {
 			for (int i = 0; i < W; i++) {
 				B[i] = 1;
 			}
-		
-			subset(0,0);
-			
-			System.out.printf("#%d %d\n",t , count);
+
+			subset(0, 0);
+
+			System.out.printf("#%d %d\n", t, count);
 		}
-		
+
 	}
+
 	static void subset(int cnt, int k) {
-		if(k >= count) return;
-		if(cnt == D) {
-			if(check()) {
+		if (k >= count)
+			return;
+		if (cnt == D) {
+			if (check()) {
 				count = Math.min(count, k);
 			}
 			return;
 		}
-		
+
 		int temp[] = new int[W];
 		temp = map[cnt];
-		
-		subset(cnt+1, k);
-		
+
+		subset(cnt + 1, k);
+
 		map[cnt] = A;
-		subset(cnt+1, k+1);
-		
+		subset(cnt + 1, k + 1);
+
 		map[cnt] = B;
-		subset(cnt+1, k+1);
-		
+		subset(cnt + 1, k + 1);
+
 		map[cnt] = temp;
-		
+
 	}
-	
-	
-//	static boolean check() {
-//		boolean pass[] = new boolean[W];
-//		
-//		int copymap[][] = new int[W][D];
-//		for (int i = 0; i < W; i++) {
-//			for (int j = 0; j < D; j++) {
-//				copymap[i][j] = map[j][i];
-//			}
-//		}
-//		for (int i = 0; i < W; i++) {
-//			int cnt =1;
-//			for (int j = 0; j < D-1; j++) {
-//				if(copymap[i][j] == copymap[i][j+1]) {
-//					cnt++;
-//				}else {
-//					cnt =1;
-//				}
-//				if(cnt >= K) {
-//					pass[i] = true;
-//					break;
-//				}
-//			}
-//		}
-//		for (int i = 0; i < W; i++) {
-//			if(!pass[i]) {
-//				return false;
-//			}
-//		}
-//		return true;
-//	}
-	
+
 	static boolean check() {
-		boolean c[] = new boolean[W];
-		int [][] newmap = new int[W][D];
-		int cnt = 1;
+		boolean pass[] = new boolean[W];
+
+		int copymap[][] = new int[W][D];
 		for (int i = 0; i < W; i++) {
 			for (int j = 0; j < D; j++) {
-				newmap[i][j]=map[j][i];
+				copymap[i][j] = map[j][i];
 			}
 		}
-		
-		
+
+		int cnt = 1;
 		for (int i = 0; i < W; i++) {
-			cnt=1;
-			for (int j = 0; j < D-1; j++) {
-				if(cnt >= K) {
-					c[i] = true;
+			cnt = 1;
+			for (int j = 0; j < D - 1; j++) {
+				if (copymap[i][j] == copymap[i][j + 1]) {
+					cnt++;
+				} else {
+					cnt = 1;
+				}
+				if (cnt >= K) {
+					pass[i] = true;
 					break;
 				}
-				if(newmap[i][j]==newmap[i][j+1]) {
-					cnt++;
-				}else {
-					cnt=1;
-				}
 			}
-			if(cnt >= K) {
-				c[i] = true;
-			}
-		}
-		
-		for (int i = 0; i < W; i++) {
-			if(!c[i]) {
+			if (!pass[i]) {
 				return false;
 			}
 		}
 		return true;
 	}
+
 }
