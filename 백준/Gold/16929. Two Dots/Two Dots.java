33aa447415a40ca7f078249c19c24e41158948ce
An Na <99172832+An0401na@users.noreply.github.com>
2023-04-11 02:11:43 +0900

    [Gold IV] Title: Two Dots, Time: 124 ms, Memory: 13904 KB -BaekjoonHub

--- 백준/Gold/16929. Two Dots/Two Dots.java
+++ 백준/Gold/16929. Two Dots/Two Dots.java
@@ -6,7 +6,7 @@ public class Main {
 	static int M;
 	static char[][] map;
 	static boolean[][] visited;
-	static int temp[][];
+	static int numbering[][];
 	static int dir[][] = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
 
 	public static void main(String[] args) throws IOException {
@@ -17,8 +17,8 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 
 		map = new char[N][M];
-		visited = new boolean[N][M];
-		temp = new int[N][M];
+//		visited = new boolean[N][M];
+		numbering = new int[N][M];
 		for (int i = 0; i < N; i++) {
 			map[i] = br.readLine().toCharArray();
 		}
@@ -26,12 +26,12 @@ public class Main {
 			for (int j = 0; j < M - 1; j++) {
 				if (map[i][j] == map[i][j + 1]) {
 					visited = new boolean[N][M];
-					temp = new int[N][M];
-					visited[i][j] = true;
-					temp[i][j] = 1;
+					numbering = new int[N][M];
+//					visited[i][j] = true;
+					numbering[i][j] = 1;
 					checkDfs(i, j, map[i][j]);
-					visited[i][j] = false;
-					temp[i][j] = 0;
+//					visited[i][j] = false;
+					numbering[i][j] = 0;
 				}
 			}
 		}
@@ -43,16 +43,16 @@ public class Main {
 		for (int i = 0; i <4; i++) {
 			int nr = r + dir[i][0];
 			int nc = c + dir[i][1];
-			if(isInRange(nr, nc) && temp[r][c]!=2 && temp[nr][nc] == 1) {
+			if(isInRange(nr, nc) && numbering[r][c]!=2 && numbering[nr][nc] == 1) {
 				System.out.println("Yes");
 				System.exit(0);
 			}
-			if (isInRange(nr, nc)&& !visited[nr][nc] && map[nr][nc] == charater) {						
-				visited[nr][nc] = true;
-				temp[nr][nc]= temp[r][c]+1;
+			if (isInRange(nr, nc)&& numbering[nr][nc]==0 && map[nr][nc] == charater) {						
+//				visited[nr][nc] = true;
+				numbering[nr][nc]= numbering[r][c]+1;
 				checkDfs(nr, nc, charater);
-				temp[nr][nc]= 0;
-				visited[nr][nc] = false;
+				numbering[nr][nc]= 0;
+//				visited[nr][nc] = false;
 			}
 		}
 	}
