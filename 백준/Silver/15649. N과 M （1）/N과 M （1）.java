7e80b6a60977215774476e9ebfd48b3f0b7abe22
An Na <99172832+An0401na@users.noreply.github.com>
2023-04-22 17:16:29 +0900

    [Silver III] Title: N과 M (1), Time: 168 ms, Memory: 20280 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,43 +1,45 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	static int N;
 	static int M;
-	static int []nums;
-	static boolean[] visited;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		nums = new int[M];
-		visited = new boolean[N];
+	static int num[];
+	static boolean visited[];
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		num = new int[M];
+		sb = new StringBuilder();
+		visited = new boolean[N+1];
 		perm(0);
+		System.out.println(sb.toString());
+
 	}
 
 	private static void perm(int cnt) {
-		if(cnt==M){
+		if (cnt == M) {
 			for (int i = 0; i < M; i++) {
-				System.out.print(nums[i]+" ");
+				sb.append(num[i]).append(" ");
 			}
-			System.out.println();
+			sb.append("\n");
 			return;
-		}else {
-			for (int i = 0; i < N; i++) {
-				if(visited[i]) {
-					continue;
-				}
-				visited[i]=true;
-				nums[cnt]=i+1;
-				perm(cnt+1);
-				nums[cnt]=0;
-				visited[i]=false;
-					
-				
-			}
-			
 		}
-		
+
+		for (int i = 1; i <= N; i++) {
+			if(visited[i]) continue;
+			visited[i] = true;
+			num[cnt] = i;
+			perm(cnt + 1);
+			visited[i] = false;
+		}
 	}
 
 }
