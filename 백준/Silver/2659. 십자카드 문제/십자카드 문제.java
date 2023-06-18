444a56fae84fd2536788633ada1903a8340b0ace
An Na <99172832+An0401na@users.noreply.github.com>
2023-06-19 01:59:58 +0900

    [Silver III] Title: 십자카드 문제, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/2659. 십자카드 문제/십자카드 문제.java
+++ 백준/Silver/2659. 십자카드 문제/십자카드 문제.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
@@ -10,7 +8,6 @@ public class Main {
 	static int num[] = new int [4];
 	static int clockNum;
 	static int minNum;
-	static int count;
 	static HashSet<Integer> set = new HashSet<>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,41 +16,43 @@ public class Main {
 		for (int i = 0; i < 4; i++) {
 			num[i] = Integer.parseInt(st.nextToken());
 		}
+		//------입력
+		
 		clockNum = 	getClockNum(num);
 		
-//		System.out.println(clockNum);
-		
 		num = new int[]{ 1, 1, 1, 1};
-		dfs(0);
+		
+		findSmallColockNum(0); 
+		
 		System.out.println(set.size());
 	}
 	
-	static void dfs(int cnt) {
+	//주어진 시계수보다 작은 시계수 찾는 함수
+	static void findSmallColockNum(int cnt) {
 		if(cnt >= 4) {
-//			System.out.println(Arrays.toString(num));
 			int minNum = getClockNum(num);
 			if(minNum <= clockNum) {
 				set.add(minNum);
-//				count++;
 			}
 			return;
 		}
 		
 		for (int i = 1; i <= 9; i++) {
-//			if(cnt != 0 && num[cnt-1] > i) continue;
 			num[cnt] = i;
-			dfs(cnt+1);
+			findSmallColockNum(cnt+1);
 		}
 		
 		
 	}
 
-	//-----시계수 찾기
+	//-----시계수 구하기
 	static int getClockNum(int[] num) {
-		int cNum = Integer.MAX_VALUE; // 시계수
+		int cNum = Integer.MAX_VALUE; // 시계수 초기화
 		for (int i = 0; i < 4; i++) {
 			int tempNum = 0;
+			
 			int idx = i;
+			
 			for (int j = 3; j >= 0; j--) {
 				if(idx > 3) {
 					idx= 0;
