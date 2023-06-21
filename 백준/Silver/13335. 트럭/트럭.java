71c8abd35a368db5ae676eae5c745c04c9c53489
An Na <99172832+An0401na@users.noreply.github.com>
2023-06-22 08:12:06 +0900

    [Silver I] Title: 트럭, Time: 96 ms, Memory: 12832 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Deque;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
@@ -27,6 +26,7 @@ public class Main {
 		for (int i = 0; i < n; i++) {
 			truck[i] = Integer.parseInt(st.nextToken());
 		}
+		//--------입력
 		
 		bridge = new LinkedList<>();
 		for (int i = 0; i < w; i++) {
@@ -34,17 +34,26 @@ public class Main {
 		}
 		
 		int time = 0;
-		int sum = 0;
-		int nextIdx = 0;
+		int sum = 0; //현재 다리 위에 있는 트럭의 무게 합
+		int nextIdx = 0; //다음순서에 다리로 출발하려는 트럭의 인덱스 번호
+		
+		/*
+		 * 다음에 다리를 건너기를 출발하려는 트럭의 인덱스를 큐에 넣는다
+		 * 트럭과 트럭사이는 -1로 표기한다.
+		 * 
+		 */
 		while (nextIdx != truck.length) {
 			
 			//Out
+			//다리를 건너 도착하는 트럭이 있다면 다리에서 out
 			int outIdx = bridge.removeFirst();
 			if(outIdx != -1) {
 				sum -= truck[outIdx];
 			}
 			
 			//In
+			//다음 대기하는 트럭이 다리위에 건너도 하중보다 작은지 확인 후 다리 in
+			//하중보다 크다면 -1을 다리로 보냄
 			if (sum + truck[nextIdx] <= L) {
 				sum += truck[nextIdx];
 				bridge.addLast(nextIdx);
@@ -54,13 +63,6 @@ public class Main {
 			}
 			
 			time++;
-			
-//			System.out.println("==== [Time] :" + time +"====");
-//			System.out.println("sum : "+ sum);
-//			for (Iterator<Integer> it = bridge.iterator(); it.hasNext();) {
-//				System.out.print(it.next()+ " ");
-//			}
-//			System.out.println("\n");
 		}
 		
 		System.out.println(time);
