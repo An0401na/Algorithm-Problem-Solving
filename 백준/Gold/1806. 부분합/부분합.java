8f2ccdb5a5ac6640212d1da399315f4049e5a521
An Na <99172832+An0401na@users.noreply.github.com>
2023-06-30 02:52:58 +0900

    [Gold IV] Title: 부분합, Time: 880 ms, Memory: 23504 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int S;
+	static int [] num;
+	static int minLen = Integer.MAX_VALUE;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		
+		num = new int[N];
+		st = new StringTokenizer(br.readLine());
+		int sum []= new int[N];
+		int s = 0;
+		for (int i = 0; i < N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+			s += num[i];
+			sum[i] = s;
+		}
+		
+		if(s < S) {
+			System.out.println(0);
+			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			
+			s = 0;
+			int idx = i;
+			while (s < S && idx < N) {
+				
+//				System.out.print(num[idx]+" ");
+				s+=num[idx++];
+			}
+//			System.out.println();
+			if(s >= S) {
+				minLen = Math.min(minLen, idx-i);
+			}
+//			System.out.println("합 :"+s);
+//			System.out.println("길이 : "+ (idx-i));
+//			System.out.println("최소 : "+ minLen);
+//			System.out.println();
+			if(sum[N-1]-sum[i] < S) break;
+		}
+		System.out.println(minLen);
+		
+		
+	}
+
+}
