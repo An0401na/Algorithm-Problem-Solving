2d53d8d050c47473de599eb3dbca531c008f5e8f
An Na <99172832+An0401na@users.noreply.github.com>
2023-07-06 01:38:24 +0900

    [Gold I] Title: 비숍, Time: 540 ms, Memory: 13776 KB -BaekjoonHub

--- 백준/Gold/1799. 비숍/비숍.java
+++ 백준/Gold/1799. 비숍/비숍.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N;
 	static int board[][];
-//	static boolean visited[][];
 	static int dir[][] = {{-1,-1},{1,-1},{1,1},{-1,1}};
 	static int black_cnt;
 	static int white_cnt;
@@ -25,78 +23,52 @@ public class Main {
 		}
 		
 
-//		visited = new boolean [N][N];
 		black_search(0,0,0);
-
-//		visited = new boolean [N][N];
 		white_search(0,1,0);
+		
 		System.out.println(black_cnt+white_cnt);
 		
 	}
 	
 	private static void black_search(int r, int c, int cnt) {
-//		System.out.println("r: c  - "+r+", "+c);
-
 		for(int i = r; i < N; i++) {
 			int j = 0;
 			if(i%2 != 0) {
 				j=1;
 			}
 			while (j < N) {
-//				if(!visited[i][j] && check(i,j) && board[i][j] == 1) {
 				if(check(i,j) && board[i][j] == 1) {
-//					visited[i][j] = true;
 					board[i][j] = 2;
 					black_search(i, j, cnt+1);
 					board[i][j] = 1;
-//					visited[i][j] = false;
 				}
 				j +=2;
 			}
 		}
 
 		black_cnt = Math.max(black_cnt, cnt);
-//		for (int i = 0; i < board.length; i++) {
-//			System.out.println(Arrays.toString(board[i]));
-//		}
-		
 	}
 	
 	private static void white_search(int r, int c, int cnt) {
-//		System.out.println("r: c  - "+r+", "+c);
-
 		for(int i = r; i < N; i++) {
 			int j = 0;
 			if(i%2 == 0) {
 				j=1;
 			}
 			while (j < N) {
-//				if(!visited[i][j] && check(i,j) && board[i][j] == 1) {
 				if(check(i,j) && board[i][j] == 1) {
-//					visited[i][j] = true;
 					board[i][j] = 2;
 					white_search(i, j, cnt+1);
 					board[i][j] = 1;
-//					visited[i][j] = false;
 				}
 				j +=2;
 			}
 		}
 
 		white_cnt = Math.max(white_cnt, cnt);
-//		for (int i = 0; i < board.length; i++) {
-//			System.out.println(Arrays.toString(board[i]));
-//		}
 		
 	}
 
-	private static void print() {
-		System.out.println("sssssssssssssssss");
-		for (int i = 0; i < board.length; i++) {
-			System.out.println(Arrays.toString(board[i]));
-		}
-	
-	}
 	private static boolean check(int r, int c) {
 		for (int i = 0; i < 4; i++) {
 			int nr = r + dir[i][0];
