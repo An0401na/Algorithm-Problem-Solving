ea97e935fd2d53d9208178a906f601e355a12044
An Na <99172832+An0401na@users.noreply.github.com>
2023-07-23 21:15:24 +0900

    [Silver II] Title: 부분수열의 합, Time: 92 ms, Memory: 11756 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,34 +1,48 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N;
+	static int N; 
 	static int S;
-	static int [] num;
-	
-	static int count=0;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static int num[];
+	static int count;
+	static boolean visited[];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
 		
-		N = sc.nextInt();
-		S = sc.nextInt();
 		num = new int[N];
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			num[i]= sc.nextInt();
+			num[i] = Integer.parseInt(st.nextToken());
 		}
-		dfs(0,-1,0);
+		visited = new boolean[N];
+//		for (int i = 0; i < N; i++) {
+//			if(num[i] == S) count++;
+//		}
+		subset(0, 0, false);
 		System.out.println(count);
 	}
-	
-	static void dfs(int depth, int idx, int s) {
-		if(s==S && depth!=0) {
-			count++;
-		}
-		if(depth==N) {
+	static void subset(int cnt, int tot, boolean flag) {
+		if(cnt >= N) {
+//			System.out.println(Arrays.toString(visited));
+			if(flag &&tot == S ) {
+//				System.out.println("ddd");
+				count++;
+			}
 			return;
 		}
-		for (int i = idx+1; i < N; i++) {
-			dfs(depth+1, i, s + num[i]);
-		}
+		visited[cnt] = false;
+		subset(cnt+1, tot, flag);
+		visited[cnt] = true;
+		subset(cnt+1, tot+num[cnt], true);
+		
+		
 	}
+
 }
