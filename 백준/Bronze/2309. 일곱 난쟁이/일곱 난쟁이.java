e085acae525ba778a8d2ff1ee7f9ae4e5f2757e6
An Na <99172832+An0401na@users.noreply.github.com>
2023-12-28 01:39:11 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 88 ms, Memory: 12924 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,45 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-	static int N=9;
-	static int M =100;
-	static int p[] = new int[9];
-	static boolean[] visited= new boolean[9];
-	static int nums[]=new int [7];
-	static int result[]= new int[7];
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		for (int i = 0; i < 9; i++) {
-			p[i] = sc.nextInt();
-		}
-		
-		combi(0, 0);
-		Arrays.sort(result);
-		for (int i = 0; i <7; i++) {
-			System.out.println(result[i]);
-		}
-	}
-	
-	static void combi(int cnt, int total) {
-		if(cnt ==7 ) {
-			if(total==100) {
-				System.arraycopy(nums, 0, result, 0, nums.length);
-				return ;
-			}
-			return;
-		}
-		for (int i = 0; i < N; i++) {
-			if(visited[i]) {
-				continue;
-			}
-			visited[i]=true;
-			nums[cnt]=p[i];
-			combi(cnt+1, total+p[i]);
-			nums[cnt]=0;
-			visited[i]= false;
-		}
-	}
 
+    static int height[];
+
+    static int result[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        height = new int[9];
+        result = new int[7];
+        for (int i = 0; i < 9; i++) {
+            height[i] = Integer.parseInt(br.readLine());
+        }
+
+        dfs(0, 0);
+
+    }
+
+    static void dfs(int idx, int start){
+        if(idx == 7){
+            int sum = 0;
+            for (int i = 0; i < 7; i++) {
+                sum += result[i];
+            }
+            if(sum == 100) {
+
+                Arrays.sort(result);
+                for (int i = 0; i < 7; i++) {
+                    System.out.println(result[i]);
+                }
+                System.exit(0);
+            }
+            return;
+        }
+        for (int i = start; i < height.length; i++) {
+            result[idx] = height[i];
+            dfs(idx+1, i+1);
+        }
+    }
 }
