4f147929457f43c57b76d4f99f14e8eb983b2acc
An Na <99172832+An0401na@users.noreply.github.com>
2023-12-28 00:39:32 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.java
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.java
@@ -14,29 +14,56 @@ public class Main {
      *
      */
     static int input[];
+    static int number[];
+    static int min; // 최소 공배수
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         input = new int[5];
-
+        number = new int[3];
+        min = Integer.MAX_VALUE;
         for (int i = 0; i < 5; i++) {
             input[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i < 1000001; i++) {
-            int cnt = 0;
-            for (int j = 0; j < 5; j++) {
-                if(i % input[j] != 0) {
-                    cnt ++;
-                    if(cnt == 3) break;
-                }
+        dfs(0, 0);
+
+        System.out.println(min);
+
+    }
+
+    public static void dfs(int idx, int start) {
+        if(idx == 3){
+            // 최소 공배수를 구해서 현재 대부분의 배수보다 더 작은 수 인지 비교
+            int lcm = getLCM(getLCM(number[0], number[1]), number[2]);
+            if(min > lcm) {
+                min = lcm;
             }
-            if(cnt < 3) {
-                System.out.println(i);
-                break;
-            }
+            return;
         }
+        for (int i = start; i < 5; i++) {
+            number[idx] = input[i];
+            dfs(idx+1, i+1);
+        }
+    }
+
+    public static int getLCM(int a, int b){
+        if( a < b ){
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        return a * b / getGCD(a, b);  // 두수의 곱을 최대 공약수로 나누면 최소공배수
+
+    }
+
+
+    public static int getGCD(int a, int b){
+        // a > b 일때 
+        if(b == 0) return a; // b가 0이면 a를 리턴 
+        if( a % b == 0) return b; // a 가 b로 나누어 떨어지면 b 리턴
+        return getGCD(b, a%b); // 나누어 떨어지지 않으면 a를 b로, b를 a에서 b로 나눈 나머지로 대입하고 재귀
     }
 }
