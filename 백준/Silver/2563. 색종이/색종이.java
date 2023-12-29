83ca06c51e2e952fb807ad8760cdb44b11766611
An Na <99172832+An0401na@users.noreply.github.com>
2023-12-30 04:23:29 +0900

    [Silver V] Title: 색종이, Time: 76 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,43 +1,78 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int[][] paper;
-	static int totalArea;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine());
-		paper = new int[101][101];
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int r = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			coloring(r, c);
-		}
 
-		
-		for (int i = 0; i < 101; i++) {
-			for (int j = 0; j < 101; j++) {
-				if(paper[i][j] == 1) {
-					totalArea++;
-				}
-			}
-		}
-		System.out.println(totalArea);
-		
-	}
-	private static void coloring(int r, int c) {
-		for (int i = r; i < r+10; i++) {
-			for (int j = c; j < c+10; j++) {
-				paper[i][j] = 1;
-			}
-		}
-	}
+    static int N;
+    static int[][] board;
+    static Paper[] papers;
+
+    static class Paper{
+        int x;
+        int y;
+
+        @Override
+        public String toString() {
+            return "Paper{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+
+        Paper(int x , int y){
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+
+    static int area;
+    static final int BLACK = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        board = new int[100][100];
+        papers = new Paper[N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            papers[i] =new Paper(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+//        System.out.println(Arrays.toString(papers));
+
+        for (int i = 0; i < N; i++) {
+            draw(papers[i].x, papers[i].y);
+        }
+
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if(board[i][j] == BLACK){
+                    area+=1;
+                }
+            }
+        }
+
+        /*for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                System.out.print(board[i][j] +" ");
+            }
+            System.out.println();
+        }*/
+
+
+        System.out.println(area);
+    }
+
+    static void draw(int x, int y){
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                board[x+i][y+j] = BLACK;
+            }
+        }
+    }
 
 }
