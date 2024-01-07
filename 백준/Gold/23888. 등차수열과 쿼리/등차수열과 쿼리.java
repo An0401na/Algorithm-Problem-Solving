8828be93e65622fbc0b73ea60b03c652ad4ff359
An Na <99172832+An0401na@users.noreply.github.com>
2024-01-08 01:32:33 +0900

    [Gold V] Title: 등차수열과 쿼리, Time: 324 ms, Memory: 15180 KB, Score: 30 point -BaekjoonHub

--- 백준/Gold/23888. 등차수열과 쿼리/등차수열과 쿼리.java
+++ 백준/Gold/23888. 등차수열과 쿼리/등차수열과 쿼리.java
@@ -4,44 +4,44 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int a;
-    static int b;
-    static int q;
+    static long a;
+    static long d;
+    static long q;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        a = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
-        q = Integer.parseInt(br.readLine());
+        a = Long.parseLong(st.nextToken());
+        d = Long.parseLong(st.nextToken());
+        q = Long.parseLong(br.readLine());
 
         for (int p = 0; p < q; p++) {
             st = new StringTokenizer(br.readLine());
 
-            int check = Integer.parseInt(st.nextToken());
-            int l = Integer.parseInt(st.nextToken());
-            int r = Integer.parseInt(st.nextToken());
+            long check = Long.parseLong(st.nextToken());
+            long l = Long.parseLong(st.nextToken());
+            long r = Long.parseLong(st.nextToken());
 
             if(check == 1){
                 System.out.println(sequenceSum(r) - sequenceSum(l-1));
             }else{
                 if(l == r){
-                    System.out.println(a + (l-1)*b);
+                    System.out.println(a + (l-1)*d);
                 }else{
-                    System.out.println(gcd(Math.max(a, b), Math.min(a, b)));
+                    System.out.println(gcd(Math.max(a, d), Math.min(a, d)));
                 }
             }
 
         }
     }
 
-    private static long gcd(int a, int b) {
+    private static long gcd(long a, long b) {
         if(b == 0) return a;
         return gcd(b, a%b);
     }
 
-    private static long sequenceSum(int n) {
-        return (n * (2*a + (n-1)*b))/2;
+    private static long sequenceSum(long n) {
+        return (n * (2*a + (n-1)*d))/2;
     }
 }
