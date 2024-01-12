59f3b19626ce1b09cc8efff9e8223bc796ce4ce5
An Na <99172832+An0401na@users.noreply.github.com>
2024-01-12 13:24:31 +0900

    [Gold IV] Title: 수들의 합 4, Time: 324 ms, Memory: 35828 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
@@ -2,49 +2,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
     static int N;
-    static int K;
-    static int[] sum;
-    static Map<Integer, Long> map = new HashMap<>();
+    static long K;
+    static long A[];
+    static long prefixSum[];
+    static HashMap<Long, Integer> prefixSumCntMap;
+    static long cnt;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long answer = 0;
 
         N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        K = Long.parseLong(st.nextToken());
 
+        A = new long[N+1];
+        prefixSum = new long[N+1];
         st = new StringTokenizer(br.readLine());
-        sum = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
-
-            // 1. 누적합 중에서 K되면 answer에 더해준다.
-            if (sum[i] == K) answer++;
+        for (int i = 1; i <= N ; i++) {
+            A[i] = Long.parseLong(st.nextToken());
         }
 
+        prefixSumCntMap = new HashMap<>();
+        prefixSumCntMap.put(0L,1); // prefixSum[i]와 K가 같은 경우
         for (int i = 1; i <= N; i++) {
-            // 3. 누적된 값이 A이고 A-K가 B라고 하자.
-            //    B에 해당하는 경우가 있는 경우(즉, map에 value가 있는 경우)만큼 곧, K가 될 수 있음을 의미한다.
-            //    왜냐하면, 지금 i까지 더한 누적합에서 B가 되는 index가 j라고 하면 sum[i] - sum[j] = K가 되기 때문이다.
-            //    즉, 갯수를 구하려면 map의 value만큼이다.
-            if(map.containsKey(sum[i] - K)) {
-                answer += map.get(sum[i] - K);
-            }
+            // 누적합 배열
+            prefixSum[i] = prefixSum[i-1] + A[i];
 
-            // 2. 누적합을 map에 추가해준다.
-            if (map.containsKey(sum[i])) {
-                map.put(sum[i], map.get(sum[i]) + 1);
-            } else {
-                map.put(sum[i], 1L);
-            }
+            // K를 만드는 수를 key로 가지는 map이 있다면 value를 반환
+            // 없다면 0을 반환
+            cnt += prefixSumCntMap.getOrDefault(prefixSum[i]-K,0);
+
+//            if(prefixSumCntMap.containsKey(prefixSum[i])){
+//                prefixSumCntMap.put(prefixSum[i], prefixSumCntMap.get(prefixSum[i])+1);
+//            }else{
+//                prefixSumCntMap.put(prefixSum[i], 1);
+//            }
+            prefixSumCntMap.put(prefixSum[i], prefixSumCntMap.getOrDefault(prefixSum[i],0)+1);
         }
 
-        System.out.println(answer);
+
+        System.out.println(cnt);
+
+
     }
 }
