acfc3349f8141f70f529d46032390741fc5366d3
An Na <99172832+An0401na@users.noreply.github.com>
2024-01-21 23:22:18 +0900

    [Gold IV] Title: 부분합, Time: 216 ms, Memory: 22768 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -4,50 +4,45 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int S;
-	static int [] num;
-	static int minLen = Integer.MAX_VALUE;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		S = Integer.parseInt(st.nextToken());
-		
-		num = new int[N];
-		st = new StringTokenizer(br.readLine());
-		int sum []= new int[N];
-		int s = 0;
-		for (int i = 0; i < N; i++) {
-			num[i] = Integer.parseInt(st.nextToken());
-			s += num[i];
-			sum[i] = s;
-		}
-		
-		if(s < S) {
-			System.out.println(0);
-			return;
-		}
-		
-		for (int i = 0; i < N; i++) {
-			
-			s = 0;
-			int idx = i;
-			
-			while (s < S && idx < N) {
-				s+=num[idx++];
-			}
-			
-			if(s >= S) {
-				minLen = Math.min(minLen, idx-i);
-			}
-			
-			if(sum[N-1]-sum[i] < S) break;
-		}
-		System.out.println(minLen);
-		
-		
-	}
+    static int N; // 배열의 요소 개수
+    static int S;
+    static int arr[]; // 입력 정수를 저장하는 배열
+    static int min = Integer.MAX_VALUE;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // N과 K를 입력으로 받음
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        // 배열 요소를 입력으로 받음
+        st = new StringTokenizer(br.readLine());
+        arr = new int[N+1];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0; // 현재 부분 배열의 시작 인덱스
+        int end = 0;   // 현재 부분 배열의 끝 인덱스
+        int sum = arr[end];
+
+        while (end < N) {
+//            System.out.println("start : "+ start+", end : " + end+", sum : "+ sum);
+            if(sum >= S){
+                min = Math.min(min, end - start+1);
+            }
+
+            if(sum < S){
+                end ++;
+                sum += arr[end];
+            }else{
+                sum -= arr[start];
+                start++;
+            }
+        }
+
+        System.out.println(min == Integer.MAX_VALUE ? 0 : min);
+    }
 }
