c34e673e5a0cd74b49863956df85578e7428ca6d
An Na <99172832+An0401na@users.noreply.github.com>
2024-01-23 01:41:54 +0900

    [Silver IV] Title: 수들의 합 2, Time: 132 ms, Memory: 15000 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -4,38 +4,46 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int  N;
-	static int M;
-	static int num[];
-	static int count;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		num = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			num[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		for (int i = 0; i < N; i++) {
-			int sum = 0;
-			for (int j = i; j < N; j++) {
-				sum += num[j];
-				if(sum == M) {
-					count++;
-					break;
-				}
-				if(sum > M) {
-					break;
-				}
-			}
-		}
-		System.out.println(count);
-	}
+    static int N;
+    static long M;
+    static int[] arr;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Long.parseLong(st.nextToken());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start =0;
+        int end = 0;
+        long sum = arr[end];
+        int cnt =0;
+        while (end < N){
+            if(sum == M){
+                cnt ++;
+            }
+
+            if(sum <= M){
+                end++;
+                if(end < N ){
+                    sum += arr[end];
+                }
+            }else{
+                sum -= arr[start];
+                start++;
+            }
+
+        }
+
+        System.out.println(cnt);
+
+
+    }
 }
