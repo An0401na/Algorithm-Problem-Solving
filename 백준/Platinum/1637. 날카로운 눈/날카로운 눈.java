ee56d552d8e6d5adb920a56069f3d295607dd0c6
An Na <99172832+An0401na@users.noreply.github.com>
2024-02-01 21:53:39 +0900

    [Platinum IV] Title: 날카로운 눈, Time: 244 ms, Memory: 21656 KB -BaekjoonHub

--- 백준/Platinum/1637. 날카로운 눈/날카로운 눈.java
+++ 백준/Platinum/1637. 날카로운 눈/날카로운 눈.java
@@ -4,14 +4,23 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static Dummy[] dummies;
+    static int N;           // 더미의 개수
+    static Dummy[] dummies; // 더미 객체 배열
 
-    static class Dummy{
-        long a;
-        long b;
-        long c;
+    // 더미 클래스 정의
+    static class Dummy {
+        long a;  // 시작 정수
+        long b;  // 정수 간격
+        long c;  // 마지막 정수
 
+        // 생성자
+        public Dummy(long a, long c, long b) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        // 디버깅을 위한 toString 메소드 오버라이드
         @Override
         public String toString() {
             return "Dummy{" +
@@ -20,80 +29,77 @@ public class Main {
                     ", c=" + c +
                     '}';
         }
-
-        public Dummy(long a,long c, long b) {
-            this.a = a;
-            this.b = b;
-            this.c = c;
-        }
     }
-    static long max = Long.MIN_VALUE;
+
+    static long max = Long.MIN_VALUE; // 더미 배열 내 최대 c값 저장
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         dummies = new Dummy[N];
+
+        // 더미 객체 배열 초기화 및 입력 받기
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             dummies[i] = new Dummy(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
-            max = Math.max(max, dummies[i].c);
+            max = Math.max(max, dummies[i].c); // 최대 c값 갱신
         }
 
-        long start = 1;
-        long end = max;
-        long resultNum = -1;
-        while (start <= end){
-//            System.out.println("start : "+ start +", end : "+ end);
+        long start = 1;       // 이분 탐색 시작값
+        long end = max;       // 이분 탐색 끝값
+        long resultNum = -1;  // 결과로 찾은 홀수개 존재하는 정수
 
-            long mid = (start + end)/2;
-//            System.out.println("mid : " + mid);
-            long numCnt = getsmallOrEqualNumCnt(mid);
+        // 이분 탐색 실행
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long numCnt = getSmallOrEqualNumCnt(mid);
 
-            if(numCnt % 2L == 1){
-//                System.out.println("1-------------------");
+            // 홀수개 존재하는 정수 찾은 경우
+            if (numCnt % 2L == 1) {
                 end = mid - 1;
                 resultNum = mid;
-            }else{
-//                System.out.println("0-----");
-                start = mid +1;
+            } else {
+                start = mid + 1;
             }
         }
 
-        if(resultNum == -1){
+        // 결과가 없는 경우 "NOTHING" 출력 후 종료
+        if (resultNum == -1) {
             System.out.println("NOTHING");
             return;
         }
-        // 가장 오른쪽거에서 - 가장 왼쪽꺼 +1
-        long cnt = getsmallOrEqualNumCnt(resultNum) - getsmallOrEqualNumCnt(resultNum-1);
 
-        System.out.println(resultNum +" "+cnt);
+        // 홀수개 존재하는 정수의 개수 계산
+        long cnt = getSmallOrEqualNumCnt(resultNum) - getSmallOrEqualNumCnt(resultNum - 1);
+
+        // 결과 출력
+        System.out.println(resultNum + " " + cnt);
     }
 
-
-
-    // 1 1 2 2 2 2 3 3 4 4 4 5 5
-    // 0 0 0 0 0 0 0 0 1 1 1
-    private static long getsmallOrEqualNumCnt(long target) {
+    // target보다 작거나 같은 숫자의 개수를 반환하는 메소드
+    private static long getSmallOrEqualNumCnt(long target) {
         long total = 0;
 
+        // 모든 더미에 대해 처리
         for (int i = 0; i < N; i++) {
-            if(target < dummies[i].a) continue;
-            if(dummies[i].c < target) {
-//                System.out.println("개수 : " + getCnt(dummies[i].c, i));
-                total += getCnt(dummies[i].c , i);
-            }else{
-//                System.out.println("개수 : " + getCnt(target, i));
+            // target보다 작은 경우 무시
+            if (target < dummies[i].a) continue;
+
+            // 더미의 c값이 target보다 작은 경우
+            if (dummies[i].c < target) {
+                // 해당 더미에서 target 이하의 숫자의 개수를 더함
+                total += getCnt(dummies[i].c, i);
+            } else {
+                // 그렇지 않으면 해당 더미에서 target 이하의 숫자의 개수를 더함
                 total += getCnt(target, i);
             }
-
-//            System.out.println("target : " + target+" total : "+total);
-
         }
 
         return total;
     }
 
+    // target 이하의 숫자의 개수를 반환하는 메소드
     private static long getCnt(long target, int i) {
         return ((target - dummies[i].a) / dummies[i].b) + 1;
     }
-
 }
