5907a5eaf49d5b1cbe394f90ccae265460881caa
An Na <99172832+An0401na@users.noreply.github.com>
2024-10-02 01:19:43 +0900

    [level 3] Title: 입국심사, Time: 107.77 ms, Memory: 99.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,23 +1,41 @@
-import java.util.Arrays;
+import java.util.*;
+
 class Solution {
     public long solution(int n, int[] times) {
+        
         long answer = 0;
         Arrays.sort(times);
-        long left = 0;
-        long right = times[times.length-1] * (long)n; //모든 사람이 가장 느리게 심사받음
-        
-        while(left <= right) {
-            long mid = (left + right) / 2;
-            long complete = 0;
-            for (int i = 0; i < times.length; i++)
-                complete += mid / times[i];
-            if (complete < n) // 해당 시간에는 모든 사람이 검사받을 수 없다.
-                left = mid + 1;
-            else {
-                right = mid - 1;
-                answer = mid; // 모두 검사받았으나, 더 최솟값이 있을 수 있다.
+        // 이분탐색 기준은 모든 인원이 입국심사를 받는데 걸리는 시간
+        // 1분 동안 모든 인원이 입국심사를 전부 받았나? -> X
+        // 2분 동안 모든 인원이 입국심사를 전부 받았나? -> X
+        // 3분 동안 모든 인원이 입국심사를 전부 받았나? -> O
+        // 4분 동안 모든 인원이 입국심사를 전부 받았나? -> O
+        // 5분 동안 모든 인원이 입국심사를 전부 받았나? -> O
+        // 의 형태를 띄기때문에 이분탐색으로 풀이 가능
+        long left = 1;
+        long right = times[0] * (long)n; // 가장 오래 걸리는 경우 (심사하는데 걸리는 시간이 가장 긴 시간 * 입국신사 기다리는 사람의 수)
+        long mid = (left + right)/2;
+        while (left <= right){
+            if(isAllPass(mid, n , times)){ // mid 시간내에 모든인원이 입국심사를 받았는지 확인
+                // 받았다면 더 짧은 시간 범위를 확인
+                right = mid -1 ;
+                answer = mid;
+            }else{
+                // 못받았다면 시간을 늘려서 더 긴 시간 범위 확인
+                left = mid +1;
             }
-        }  
+            mid = (left + right)/2;            
+        }
         return answer;
+    }
+    public boolean isAllPass(long time, int n, int[] times){
+        int pass = 0;
+        for(int i = 0; i < times.length; i ++){
+            pass += time/times[i];
+            if(pass >= n){
+                return true;
+            }
+        }
+        return false;
     }
 }
