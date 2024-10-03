1edb731218d9d63a43407650bf2cbe2e77eec8b3
An Na <99172832+An0401na@users.noreply.github.com>
2024-10-04 03:05:51 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 27.52 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/README.md
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 퍼즐 조각 채우기 - 84021 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84021) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84021#qna) 
 
 ### 성능 요약
 
-메모리: 83.1 MB, 시간: 7.36 ms
+메모리: 78.8 MB, 시간: 27.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 03일 23:11:22
+2024년 10월 04일 03:05:45
 
 ### 문제 설명
 
--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
@@ -1,143 +1,186 @@
 import java.util.*;
-
-class Block{
-    int cnt;//칸의 개수
-    int[][] shape = new int[6][2];//모양
-}
-
 class Solution {
     
-    public int solution(int[][] game_board, int[][] table) {
-        int answer = 0;
-        
-        //1. 퍼즐 찾기
-        List<Block> puzzles = new LinkedList<>();
-        findBlocks(table, puzzles, 1, 0);
-        
-        //2. 빈칸 찾기
-        List<Block> blanks = new ArrayList<>();
-        findBlocks(game_board, blanks, 0, 1);
-        
-        //3. 빈칸에 퍼즐 넣을 수 있는지 보기
-        for(Block blank : blanks){
-            for(Block puzzle : puzzles){
-                if(canInsert(puzzle, blank)){
-                    answer+=puzzle.cnt;
-                    puzzles.remove(puzzle);
-                    //사용할 퍼즐이 더이상 없다면 종료
-                    if(puzzles.size() == 0){
-                        return answer;
+        static int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
+        static class Point{
+            int r;
+            int c;
+            public Point(int r, int c){
+                this.r = r;
+                this.c = c;
+            }
+
+            @Override
+            public String toString() {
+                return "Point{" +
+                        "r=" + r +
+                        ", c=" + c +
+                        '}';
+            }
+        }
+
+        static boolean isUsed[] = new boolean[1000]; // 보드의 길이가 최대 50이니까 퍼즐의 개수 최대 25개, 0인덱스 안쓰니까 +1
+        static int puzzleSize[] = new int[1000];
+        static int[][] gameBoard;
+        static int[][] table;
+        static int[][][] tables;
+        static int N;
+        public int solution(int[][] g, int[][] t) {
+            gameBoard = g;
+            table = t;
+
+            N = table.length;
+            int puzzleNum = 2; //0, 1은 이미 사용중이니까 2부터 표기
+
+
+            // 퍼즐 번호 매기기
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if(table[i][j] ==1){
+                        coloring(i, j, puzzleNum++);
                     }
-                    break;
                 }
             }
-        }
-            
-        return answer;
-    }
-    
-    public void findBlocks(int[][] map, List<Block> list, int flag, int nflag){
-        int n = map.length;
-        int m = map[0].length;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(map[i][j] == flag){
-                    list.add(findOne(map, n, m, i, j, flag, nflag));
+
+
+            // ㅗ , ㅏ, ㅜ, ㅓ  한바퀴 돌린 보드 네개 만들기
+            tables = new int[4][N][N];
+            tables[0] = table;
+            turnTable(table);
+            for (int i = 1; i <= 3; i++) {
+                tables[i] = turnTable(tables[i-1]);
+            }
+
+
+
+
+            /*
+            game_board를 쭉 가면서 0을 만나면 테이블 탐색
+            테이블에서 1이 아닌 숫자가 나오면 사용을 했는지 확인
+            그 모양과 현재 게임보드 0과 모양이 같은지 확인
+            => 큐에 담아서 확이
+            같지 않다면 회전한 퍼즐에서도 확인
+            다시 탐색하지 않도록 보드에서 0을 1로 칠하고 넘기기
+            같다면 테이블에 찾은 퍼즐을 isused 체크하고
+            넘기기
+             */
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if(gameBoard[i][j] == 0){ // 게임보드가 비어있다면 탐색
+                        // 게임보드에 비어있는 모양대로 큐에 담으면서 보드 매꾸기 0 -> 1 (다시 탐색하지 않도록)
+                        ArrayList<Point> q = addEmptyShape(i, j);
+                        for (int k = 0; k < 4; k++) { // 회전된 4개의 테이블의 퍼즐들 확인
+                            int fitPuzzleNum = findPuzzle(q, tables[k]); // 테이블에 들어맞는 퍼즐이 있다면 그 퍼즐 번호를 리턴/ 퍼즐이 없다면 0리턴
+                            if(fitPuzzleNum != 0) {
+                                isUsed[fitPuzzleNum] = true;
+                                break;
+                            }
+                        }
+                    }
+
                 }
             }
-        }
-    }
-    
-    //block 하나 찾기
-    public Block findOne(int[][] map, int n, int m, int x, int y, int flag, int blank){
-        int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
-        Block block = new Block();
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x, y});
-        map[x][y] = blank;
-        
-        while(!q.isEmpty()){
-            int[] now = q.poll();
-            block.shape[block.cnt++] = now;
-            for(int i=0; i<4; i++){
-                int nx = now[0] + dx[i];
-                int ny = now[1] + dy[i];
-                if(nx<0 || nx>=n || ny<0 || ny>=m || map[nx][ny] == blank){
-                    continue;
-                }
-                map[nx][ny] = blank;
-                q.add(new int[]{nx, ny});
-            }
-        }
-        
-        //해당 블럭에서 가장 앞에 있는 블럭을 0,0으로 맞추기
-        setCenter(block);
-        return block;
-    }
-    
-    public boolean canInsert(Block puzzle, Block blank){
-        if(puzzle.cnt != blank.cnt){return false;}
-        
-        int size = puzzle.cnt;
-        
-        for(int i=0; i<3; i++){
-            //맞는 퍼즐 찾으면 return true, 아니면 회전시켜보기
-            if(isSameShape(size, puzzle.shape, blank.shape)){
-                return true;
-            }
-            turn(puzzle);
-        }
-        if(isSameShape(size, puzzle.shape, blank.shape)){
-            return true;
-        }
-        return false;
-    }
-    
-    public boolean isSameShape(int size, int[][] shape1, int[][] shape2){
-        for(int j=0; j<size; j++){
-            int k = 0;
-            for(; k<size; k++){
-                if(shape1[j][0] == shape2[k][0]
-                   && shape1[j][1] == shape2[k][1]){
-                    break;
+
+            int answer = 0;
+            for (int i = 0; i < 1000; i++) {
+                if(isUsed[i]){
+                    answer += puzzleSize[i];
                 }
             }
-            if(k==size){
+            return answer;
+        }
+
+        private int findPuzzle(ArrayList<Point> emptyShape, int[][] table) {
+            boolean isCheck[] = new boolean[1000];
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if(table[i][j] > 1 && !isCheck[table[i][j]]){
+                        isCheck[table[i][j]] = true; // 확인한 퍼즐은 보지 않기 위해서 체크
+                        
+                        if(!isUsed[table[i][j]] && isFit(emptyShape, i, j, table)){
+                            return table[i][j];
+                        }
+                    }
+                }
+            }
+            return 0;
+        }
+
+        private boolean isFit(ArrayList<Point> emptyShape, int r, int c, int[][] table) {
+            // 사이즈가 다르면 퍼즐 안맞음
+            if(puzzleSize[table[r][c]] != emptyShape.size()){
                 return false;
             }
-        }
-        return true;
-    }
-    
-    //90도 회전
-    public void turn(Block block){
-        for(int i=0; i<block.cnt; i++){
-            int temp = -block.shape[i][0];
-            block.shape[i][0] = block.shape[i][1];
-            block.shape[i][1] = temp;
-        }
-        setCenter(block);
-    }
-    
-    public void setCenter(Block block){
-        int minX = Integer.MAX_VALUE;
-        int minY = Integer.MAX_VALUE;
-        
-        //해당 블럭에서 가장 앞에 있는 블럭 찾기
-        for(int i=0; i<block.cnt; i++){
-            if(block.shape[i][0] < minX){
-                minX = block.shape[i][0];
-                minY = block.shape[i][1];
+            for (Point p : emptyShape) {
+                int nr = r + p.r;
+                int nc = c + p.c;
+                if(!isInRange(nr, nc) || (table[nr][nc] != table[r][c])){
+                    return false;
+                }
             }
-            else if(block.shape[i][0] == minX && block.shape[i][1] < minY){
-                minY = block.shape[i][1];
+
+            return true;
+        }
+
+
+        // r, c 점을 기준으로 보드에 비어있는 퍼즐 모양을 큐에 담고(단 상대적인 좌표로 담음), 탐색한 퍼즐 모양은 재탐색을 방지하기 위해 1로 바꿈
+        private ArrayList<Point> addEmptyShape(int r, int c) {
+            ArrayList<Point> emptyShape = new ArrayList<>();
+            Queue<Point> q = new LinkedList<>();
+
+
+            q.add(new Point(r, c));
+            while (!q.isEmpty()){
+                Point p = q.poll();
+                if(gameBoard[p.r][p.c] == 1) continue;
+                gameBoard[p.r][p.c] = 1;
+                emptyShape.add(new Point(p.r-r, p.c-c));
+                for (int d = 0; d < 4; d++) {
+                    int nr = p.r + dir[d][0];
+                    int nc = p.c + dir[d][1];
+                    if(isInRange(nr, nc) && gameBoard[nr][nc] == 0){
+                        q.add(new Point(nr, nc));
+                    }
+                }
             }
+
+            return  emptyShape;
         }
-        
-        for(int i=0; i<block.cnt; i++){
-            block.shape[i][0] -= minX;
-            block.shape[i][1] -= minY;
+
+        // 테이블에 들어맞는 퍼즐이 있다면 그 퍼즐 번호를 리턴/ 퍼즐이 없다면 0리턴
+        private int[][] turnTable(int[][] table) {
+            int newTable[][] = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    newTable[j][N-i-1] = table[i][j];
+                }
+            }
+            return  newTable;
         }
-    }
-    
+
+
+        private void coloring(int r, int c, int num) {
+            Queue<Point> q = new LinkedList<>();
+            int size = 0;
+            q.add(new Point(r, c));
+            table[r][c] = num;
+            while (!q.isEmpty()){
+                Point p = q.poll();
+                size +=1;
+                for (int d = 0; d < 4; d++) {
+                    int nr = p.r + dir[d][0];
+                    int nc = p.c + dir[d][1];
+                    if(isInRange(nr, nc) && table[nr][nc] == 1){
+                        table[nr][nc] = num;
+                        q.add(new Point(nr, nc));
+                    }
+                }
+            }
+            puzzleSize[num] = size;
+        }
+
+        private boolean isInRange(int nr, int nc) {
+            return 0 <= nr && nr < N && 0 <= nc && nc < N;
+        }
 }
