a4338eb3740a23a59398f5d6f9f69722bc07bc09
An Na <99172832+An0401na@users.noreply.github.com>
2024-10-11 00:34:00 +0900

    [Gold III] Title: 팰린드롬 만들기, Time: 284 ms, Memory: 111072 KB -BaekjoonHub

--- 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,13 +13,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         nums = new int[N];
-        dp = new int[N][N];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        dp = new int[N][N];
         for (int i = 0; i < N; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
-            for(int j=0; j< N; ++j){
-                dp[i][j] = -1;
-            }
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
 
 
@@ -26,18 +25,22 @@ public class Main {
         System.out.println(result);
     }
 
-    private static int recur(int left, int right) {
-        if(right <= left){
+    private static int recur(int left, int right){
+        if(left > right){
             return 0;
         }
+        if(dp[left][right] != Integer.MAX_VALUE) return dp[left][right];
 
-        if(dp[left][right] != -1)   return dp[left][right];
-
-        if(nums[right] == nums[left]){
-            return dp[left][right] = recur(left + 1, right - 1);
+        int value = Integer.MAX_VALUE;
+        if(nums[left] != nums[right]){
+            value = Math.min(1+recur(left+1, right),
+                    1+recur(left, right-1));
+        }else{
+            value = recur(left+1, right-1);
         }
 
-        return dp[left][right] = Math.min(recur(left + 1, right), recur(left, right - 1)) + 1;
+        return dp[left][right]=value;
     }
+
 
 }
