8cee292c714559a9e7caea3817743b793d1f81f5
An Na <99172832+An0401na@users.noreply.github.com>
2024-10-16 23:20:25 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 41.34 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -29,6 +29,7 @@ class Solution {
     // alp, cop에서 maxAlp와 MaxCop를 만드는 최소 시간을 리턴하는 재귀 함수
     
         private int recur(int alp, int cop){
+            
             if(alp >= maxAlp && cop >= maxCop) return 0;
 
             alp = Math.min(maxAlp, alp);
@@ -37,25 +38,28 @@ class Solution {
             if(dp[alp][cop] != Integer.MAX_VALUE) return dp[alp][cop];
 
             int value = Integer.MAX_VALUE;
-
-            if(alp < maxAlp)    value = Math.min(value, recur(alp + 1, cop) + 1);
-            if(cop < maxCop)    value = Math.min(value, recur(alp, cop + 1) + 1);
-
-
             for(int i = 0; i < problems.length; i++){
                 int alp_req = problems[i][0];
                 int cop_req = problems[i][1];
                 int alp_rwd = problems[i][2];
                 int cop_rwd = problems[i][3];
                 int cost = problems[i][4];
+                int alp_cost= 0;
+                int cop_cost = 0;
 
-                if(alp >= alp_req && cop >= cop_req) {
-                    if((alp >= maxAlp && cop_rwd == 0) || (cop >= maxCop && alp_rwd == 0)) continue;
-                    value = Math.min(value, recur(alp + alp_rwd, cop + cop_rwd) + cost);
+                if((alp >= maxAlp && cop_rwd == 0) || (cop >= maxCop && alp_rwd == 0)) continue;
+
+                if(alp < alp_req){
+                    value = Math.min(value , recur(alp + 1, cop) + 1);
+                    alp_cost = alp_req - alp;
                 }
+                if(cop < cop_req){
+                    value = Math.min(value , recur(alp, cop + 1) + 1);
+                    cop_cost = cop_req - cop;
+                }
+                value = Math.min(value , recur(alp + alp_cost+ alp_rwd, cop+ cop_cost +cop_rwd)
+                            + cost + alp_cost + cop_cost );
             }
-
-
             return dp[alp][cop]= value;
         }
 }
