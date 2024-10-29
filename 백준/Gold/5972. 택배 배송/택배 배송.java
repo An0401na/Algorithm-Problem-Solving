1b4eee25daf77c24965de4986d0fcf3abf21dcdd
An Na <99172832+An0401na@users.noreply.github.com>
2024-10-30 00:13:02 +0900

    [Gold V] Title: 택배 배송, Time: 348 ms, Memory: 40080 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -7,64 +7,65 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int dist[];
-    static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-    static int N,M;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st =new StringTokenizer(br.readLine() );
-        N = Integer.parseInt(st.nextToken());   //헛간 그래프
-        M = Integer.parseInt(st.nextToken());   //간선
-
-        dist = new int[N+1];
-
-        for (int i = 0; i <= N ; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for(int i =0;i<M;i++){
-            st= new StringTokenizer(br.readLine());
-
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            int C = Integer.parseInt(st.nextToken());
-
-            graph.get(A).add(new Node(B,C));
-            graph.get(B).add(new Node(A,C));
-        }
-
-        Arrays.fill(dist,Integer.MAX_VALUE);
-
-        dijst();
-        System.out.println(dist[N]);
-    }
-
-    static void dijst(){
-        PriorityQueue<Node> queue = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
-        queue.add(new Node(1,0));
-        dist[1] = 0;
-
-        while (!queue.isEmpty()){
-              Node now = queue.poll();
-
-              for(Node next : graph.get(now.v)){
-                  if(dist[next.v] > dist[now.v]+next.cost){
-                      dist[next.v] =dist[now.v]+next.cost;
-                      queue.add(new Node(next.v,dist[next.v]));
-                  }
-              }
-        }
-    }
-
-    static class Node{
+    static int N;
+    static int M;
+    static ArrayList<Node>[] graph;
+    static class Node implements Comparable<Node> {
         int v;
         int cost;
 
-        public Node(int v,int cost){
-                this.v = v;
-                this.cost = cost;
+        public Node(int v, int cost){
+            this.v = v;
+            this.cost = cost;
         }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N+1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph[v].add(new Node(w, cost));
+            graph[w].add(new Node(v, cost));
+        }
+
+        int dist[] = new int[N+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[1] = 0;
+
+        PriorityQueue<Node> pq= new PriorityQueue<>();
+        pq.add(new Node(1, 0));
+
+        while (!pq.isEmpty()){
+            Node now = pq.poll();
+            
+            for (Node next : graph[now.v]){
+                if(dist[next.v] > dist[now.v] + next.cost){
+                    dist[next.v] = dist[now.v] + next.cost;
+                    pq.add(new Node(next.v, dist[next.v]));
+                }
+            }
+        }
+
+        System.out.println(dist[N]);
+
+
+
     }
 }
