985420ddb00ea9f7d942a850ac5302dc2319771e
An Na <99172832+An0401na@users.noreply.github.com>
2024-10-30 20:37:47 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 472 ms, Memory: 66492 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -26,6 +26,8 @@ public class Main {
     static ArrayList<Node>[] graph;
     static int v1;
     static int v2;
+    static int[] distV1;
+    static int[] distV2;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -33,8 +35,8 @@ public class Main {
         E = Integer.parseInt(st.nextToken());
 
 
-        graph = new ArrayList[N+1];
-        for (int i = 0; i < N+1; i++) {
+        graph = new ArrayList[N + 1];
+        for (int i = 0; i < N + 1; i++) {
             graph[i] = new ArrayList<>();
         }
 
@@ -53,26 +55,37 @@ public class Main {
         v1 = Integer.parseInt(st.nextToken());
         v2 = Integer.parseInt(st.nextToken());
 
+        if (E == 0) {
+            System.out.println(-1);
+            return;
+        }
 
+        distV1 = dijkstra(v1);
+        distV2 = dijkstra(v2);
 
-        int min = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, N);
-        min = Math.min(min, dijkstra(1, v2)+ dijkstra(v2, v1)+ dijkstra(v1, N));
+        if (distV1[1] == Integer.MAX_VALUE || distV1[v2] == Integer.MAX_VALUE || distV2[N] == Integer.MAX_VALUE){
+            System.out.println(-1);
+            return;
+        }
+
+        int min = distV1[1] + distV1[v2] + distV2[N];
+        min = Math.min(min,distV2[1] + distV2[v1] + distV1[N]);
+        
 
         System.out.println(min);
 
     }
 
-    private static int dijkstra(int start, int end) {
-        boolean visited[] = new boolean[N+1];
+    private static int[] dijkstra(int start) {
         int dist[] = new int[N+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[start] = 0;
+
         PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.add(new Node(start, 0));
+
         while (!pq.isEmpty()){
             Node now = pq.poll();
-            if(visited[now.v]) continue;
-            visited[now.v] = true;
             if(dist[now.v] != now.len) continue;
             for (Node next : graph[now.v]){
                 if(dist[next.v] > dist[now.v] + next.len){
@@ -81,11 +94,8 @@ public class Main {
                 }
             }
         }
-
-        if(dist[end] == Integer.MAX_VALUE) {
-            System.out.println(-1);
-            System.exit(0);
-        }
-        return dist[end];
+        
+        
+        return dist;
     }
 }
