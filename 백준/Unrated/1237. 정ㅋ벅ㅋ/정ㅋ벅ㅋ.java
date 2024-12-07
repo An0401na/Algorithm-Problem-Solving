5695bd20908a7a00cc3c49a4bd5a36c6fd4ed5a9
An Na <99172832+An0401na@users.noreply.github.com>
2024-12-08 02:37:32 +0900

    [Unrated] Title: 정ㅋ벅ㅋ, Time: 60 ms, Memory: 11200 KB -BaekjoonHub

---
+++ 백준/Unrated/1237. 정ㅋ벅ㅋ/정ㅋ벅ㅋ.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("문제의 정답");
+    }
+
+}
