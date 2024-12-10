0d238051665473d2f687ab6044401653b3becc81
An Na <99172832+An0401na@users.noreply.github.com>
2024-12-11 00:03:19 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 64 ms, Memory: 11212 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+
+    }
+
+}
